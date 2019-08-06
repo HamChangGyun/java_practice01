f92b2b97d823bdac2f313a2b250755539e73f1d7
임승범 <seungbeom123@naver.com>
2019-08-06 16:26:55 +0900

    1, 2, 3, 4, 5 완료

--- src/practice01/prob5/Prob5.java
+++ src/practice01/prob5/Prob5.java
@@ -2,6 +2,21 @@ package practice01.prob5;
 
 public class Prob5 {
 	public static void main (String args[]) {
-		
+		for(int i=1; i<100; i++)
+		{
+			if((i % 10 == 3)||(i % 10 == 6)||(i % 10 == 9)||(i/10 ==3)||(i/10 ==6)||(i/10 ==9)) {
+				System.out.print(i + "짝");
+				if(((i/10 == 3)&&(i%10 == 3))||((i/10 == 3)&&(i%10 == 6))||((i/10 == 3)&&(i%10 == 9))) {
+					System.out.print("짝");
+				}
+				if(((i/10 == 6)&&(i%10 == 3))||((i/10 == 6)&&(i%10 == 6))||((i/10 == 6)&&(i%10 == 9))) {
+					System.out.print("짝");
+				}
+				if(((i/10 == 9)&&(i%10 == 3))||((i/10 == 9)&&(i%10 == 6))||((i/10 == 9)&&(i%10 == 9))) {
+					System.out.print("짝");
+				}
+				System.out.println();					
+			}
+		}
 	}
 }
